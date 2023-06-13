17c00039aca9ff50f4a955dcee4594c535bdde2e
Yasmina <yasmina.andrade@yahoo.com.br>
2023-06-13 09:49:26 -0400

    create index.js

--- index.js
+++ index.js
@@ -1,12 +1,85 @@
-const express = require('express')
-const app = express()
+/*const express = require('express');
+const app = express();
+
+const port = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
-app.get('/oi', function (req, res) {
-    res.send('Olá, mundo!')
-})
+app.get('/oi', (req, res) => {
+    res.send('Olá, mundo!');
+});
 
-app.listen(3000)
+app.listen(port, () => {
+  console.info(`Está disponível em http://localhost:${port}`);
+});*/
+
+const express = require("express");
+const app = express();
+
+// Indicamos que o Express deve considerar o Body
+// das requisições como JSON
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/oi", function (req, res) {
+  res.send("Olá, mundo!");
+});
+
+// Lista de heróis
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//              0                    1                2
+
+// Read All - [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois.filter(Boolean));
+});
+
+// Create - [POST] /herois
+app.post("/herois", function (req, res) {
+  // console.log(req.body, typeof req.body);
+
+  const nome = req.body.nome;
+  // console.log(nome, typeof nome);
+
+  herois.push(nome);
+
+  res.send("Item criado com sucesso!");
+});
+
+// Read By Id - [GET] /herois/:id
+app.get("/herois/:id", function (req, res) {
+  const id = req.params.id;
+
+  const item = herois[id - 1];
+
+  res.send(item);
+});
+
+// Update - [PUT] /herois/:id
+app.put("/herois/:id", function (req, res) {
+  const id = req.params.id;
+
+  const novoNome = req.body.nome;
+
+  herois[id - 1] = novoNome;
+
+  res.send("Item atualizado com sucesso!");
+});
+
+// Delete - [DELETE] /herois/:id
+app.delete("/herois/:id", function (req, res) {
+  const id = req.params.id;
+
+  delete herois[id - 1];
+
+  res.send("Item removido com sucesso!");
+});
+
+app.listen(3000, function () {
+  console.log("Aplicando rodando em http://localhost:3000");
+});
